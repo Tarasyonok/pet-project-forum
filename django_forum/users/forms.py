bbd38a6f9d565b9da6e1b309c47074ce08de6448
Kir Tarasov <kir_tarasov@mail.ru>
2025-10-16 02:48:26 +0300

    ✨ Users views & forms

---
+++ django_forum/users/forms.py
@@ -0,0 +1,62 @@
+import django.conf
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+import django.core.exceptions
+from django import forms
+import django.utils.timezone
+from users.models import UserProfile
+
+User = get_user_model()
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["email", "password1", "password2", "username"]
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name"]
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ["avatar", "bio", "birthday"]
+        widgets = {
+            "birthday": forms.DateInput(attrs={"type": "date"}),
+            "bio": forms.Textarea(attrs={"rows": 4}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        self.user_instance = kwargs.pop("user_instance", None)
+        super().__init__(*args, **kwargs)
+
+        if args or kwargs.get("data"):
+            self.user_form = UserEditForm(data=kwargs.get("data"), instance=self.user_instance, prefix="user")
+        else:
+            self.user_form = UserEditForm(instance=self.user_instance, prefix="user")
+
+    def is_valid(self):
+        user_valid = self.user_form.is_valid()
+        profile_valid = super().is_valid()
+        return user_valid and profile_valid
+
+    def clean_avatar(self):
+        avatar = self.cleaned_data.get("avatar")
+        if avatar and avatar.size > 5 * 1024 * 1024:
+            raise django.forms.ValidationError("Max file size is 5MB")
+        return avatar
+
+    def save(self, *, commit=True):
+        user = self.user_form.save(commit=commit)
+
+        profile = super().save(commit=False)
+        profile.user = user
+
+        if commit:
+            profile.save()
+
+        return profile
---
+++ django_forum/users/views.py
@@ -0,0 +1,95 @@
+import django.urls
+from core.rep_rules import REPUTATION_RULES
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.views.generic import DetailView, FormView, UpdateView
+from users.forms import ProfileEditForm, SignUpForm
+
+User = get_user_model()
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = SignUpForm
+    success_url = django.urls.reverse_lazy("users:login")
+
+    def form_valid(self, form):
+        new_user = form.save(commit=False)
+        new_user.is_active = True
+        new_user.save()
+        return super().form_valid(form)
+
+
+class UserLoginView(LoginView):
+    def get_success_url(self):
+        return django.urls.reverse("users:profile", kwargs={"username": self.request.user.username})
+
+
+class PublicProfileView(DetailView):
+    template_name = "users/public_profile.html"
+    model = User
+    slug_field = "username"
+    slug_url_kwarg = "username"
+    context_object_name = "profile_user"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.object
+
+        context["questions_count"] = user.questions.count()
+        context["answers_count"] = user.answers.count()
+        context["reviews_count"] = user.reviews.count()
+        context["accepted_answers"] = user.answers.filter(is_accepted=True).count()
+
+        context["user_questions"] = (
+            user.questions.select_related("author__profile").prefetch_related("answers").order_by("-created_at")[:10]
+        )
+        context["user_answers"] = user.answers.select_related(
+            "question",
+            "question__author",
+            "author__profile",
+        ).order_by("-created_at")[:10]
+        context["user_reviews"] = user.reviews.select_related("author__profile").order_by("-created_at")[:10]
+
+        question_upvotes = sum(question.get_upvotes().count() for question in user.questions.all())
+        answer_upvotes = sum(answer.get_upvotes().count() for answer in user.answers.all())
+        review_upvotes = sum(review.get_upvotes().count() for review in user.reviews.all())
+        accepted_answers = user.answers.filter(is_accepted=True).count()
+
+        context["question_upvotes"] = question_upvotes * REPUTATION_RULES["question_upvote"]
+        context["answer_upvotes"] = answer_upvotes * REPUTATION_RULES["answer_upvote"]
+        context["review_upvotes"] = review_upvotes * REPUTATION_RULES["review_upvote"]
+        context["accepted_points"] = accepted_answers * REPUTATION_RULES["answer_accepted"]
+
+        context["total_earned"] = (
+            context["question_upvotes"]
+            + context["answer_upvotes"]
+            + context["review_upvotes"]
+            + context["accepted_points"]
+        )
+
+        context["is_owner"] = self.request.user == user
+        return context
+
+
+class PrivateProfileEditView(LoginRequiredMixin, UpdateView):
+    template_name = "users/profile_edit.html"
+    form_class = ProfileEditForm
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["user_instance"] = self.request.user
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if "form" not in context:
+            context["form"] = self.get_form()
+        return context
+
+    def get_success_url(self):
+        return django.urls.reverse("users:profile", kwargs={"username": self.request.user.username})
