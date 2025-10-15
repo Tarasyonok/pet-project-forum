a71b93a2737c19ab0a94ac8ba8db2a0df28f77d3
Kir Tarasov <kir_tarasov@mail.ru>
2025-10-16 02:17:32 +0300

    🔒 Username & Email auth backend

---
+++ django_forum/users/utils/backends.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class EmailAuthBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None):
+        try:
+            user = User.objects.get(email=username)
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return None
+
+        if user.check_password(password):
+            return user
+
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return None
