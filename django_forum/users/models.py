95e3f19aed3c50b9b3e948a7ff61ff8128965656
Kir Tarasov <kir_tarasov@mail.ru>
2025-10-16 02:14:10 +0300

    🗃️ User & Profile models

---
+++ django_forum/users/models.py
@@ -0,0 +1,59 @@
+import django.conf
+import users.utils.validators
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
+from django.db import models
+
+
+class User(django.contrib.auth.models.AbstractUser):
+    class Role(models.TextChoices):
+        GUEST = "G", "Guest"
+        STUDENT = "S", "Student"
+        ADMIN = "A", "Admin"
+
+    email = models.EmailField(blank=False, unique=True)
+    role = models.CharField(choices=Role.choices, default=Role.GUEST, max_length=1)
+
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self):
+        return f"User {self.username}"
+
+    def save(self, *args, **kwargs):
+        is_new = self.id is None
+
+        super().save(*args, **kwargs)
+
+        if is_new:
+            UserProfile.objects.get_or_create(user=self)
+
+
+def avatar_upload_to(instance, filename: str) -> str:
+    return f"users/{instance.user.username}/{filename}"
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        django.conf.settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    avatar = models.ImageField(
+        upload_to=avatar_upload_to,
+        blank=True,
+        validators=[
+            django.core.validators.FileExtensionValidator(["png", "jpg", "jpeg"]),
+        ],
+    )
+    bio = models.TextField(blank=True)
+    reputation_points = models.IntegerField(default=0, db_index=True)
+    birthday = models.DateField(
+        blank=True,
+        null=True,
+        validators=[
+            users.utils.validators.YearRangeValidator(2000, -10),
+        ],
+    )
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
---
+++ django_forum/users/utils/validators.py
@@ -0,0 +1,30 @@
+import datetime
+
+import django.utils.deconstruct
+import django.utils.timezone
+from django.core.exceptions import ValidationError
+
+
+@django.utils.deconstruct.deconstructible
+class YearRangeValidator:
+    def __init__(self, min_year, years_offset_from_current=0):
+        self.min_year = min_year
+        self.years_offset = years_offset_from_current
+
+    def __call__(self, value):
+        current_year = django.utils.timezone.now().year
+        max_year = current_year + self.years_offset
+        if isinstance(value, (datetime.date, datetime.datetime)):
+            value = value.year
+
+        if not (self.min_year <= value <= max_year):
+            raise django.core.exceptions.ValidationError(
+                self.get_error_message(current_year),
+                params={"value": value},
+            )
+
+    def get_error_message(self, current_year):
+        if self.years_offset > 0 or self.years_offset < 0:
+            return f"Year must be between {self.min_year} and {current_year + self.years_offset}."
+
+        return f"Year must be exactly {current_year}."
