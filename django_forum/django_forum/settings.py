fa9ac5a181b4837de173b1bdea389d8de76a38ae
Kir Tarasov <kir_tarasov@mail.ru>
2025-10-16 01:50:57 +0300

    🔧 Start Django project

---
+++ django_forum/django_forum/settings.py
@@ -0,0 +1,167 @@
+import pathlib
+
+import decouple
+import django.urls
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+
+SECRET_KEY = decouple.config("DJANGO_SECRET_KEY", default="FAKE-SECRET-KEY")
+DEBUG = decouple.config("DJANGO_DEBUG", default=False, cast=bool)
+ALLOWED_HOSTS = decouple.config("DJANGO_ALLOWED_HOSTS", default="*", cast=lambda v: v.split(","))
+
+
+INSTALLED_APPS = [
+    "users.apps.UsersConfig",  # Before django.contrib.auth
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "core.apps.CoreConfig",
+    "forum.apps.ForumConfig",
+    "reviews.apps.ReviewsConfig",
+    "votes.apps.VotesConfig",
+    "home.apps.HomeConfig",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    'django.middleware.locale.LocaleMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "django_forum.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "django_forum.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+LANGUAGE_CODE = "en"
+LANGUAGES = [
+    ('en', 'English'),
+    ('ru', 'Русский'),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static_dev",
+]
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+if DEBUG:
+    INSTALLED_APPS.append("debug_toolbar")
+    MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+        *MIDDLEWARE,
+    ]
+    INTERNAL_IPS = ["*"]
+
+AUTH_USER_MODEL = "users.User"
+AUTHENTICATION_BACKENDS = [
+    "users.utils.backends.EmailAuthBackend",
+]
+
+LOGIN_URL = django.urls.reverse_lazy("users:login")
+LOGIN_REDIRECT_URL = "users:profile"
+LOGOUT_REDIRECT_URL = django.urls.reverse_lazy("users:login")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "simple"},
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "django_tests.log",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "file"],
+            "level": "INFO",
+            "propagate": True,
+        },
+        "users.tests": {
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "users": {
+            "handlers": ["console", "file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
---
+++ django_forum/django_forum/urls.py
@@ -0,0 +1,22 @@
+import django.contrib.admin
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.urls import include, path
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+urlpatterns += i18n_patterns(
+    path("", include("home.urls")),
+    path("users/", include("users.urls")),
+    path("auth/", include("django.contrib.auth.urls")),
+    path("forum/", include("forum.urls")),
+    path("reviews/", include("reviews.urls")),
+    path("votes/", include("votes.urls")),
+    path("leaderboards/", include("leaderboards.urls")),
+    path("admin/", django.contrib.admin.site.urls),
+    *static(django.conf.settings.STATIC_URL, document_root=django.conf.settings.STATIC_ROOT),
+)
+
+handler404 = 'home.views.custom_404_view'
