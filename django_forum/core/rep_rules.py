be5deaccb3d6410b1677aff684a8faead409f6d7
Kir Tarasov <kir_tarasov@mail.ru>
2025-10-16 02:52:16 +0300

    🔧 Reputation rules

---
+++ django_forum/core/rep_rules.py
@@ -0,0 +1,10 @@
+# Reputation points system
+REPUTATION_RULES = {
+    "question_upvote": 5,
+    "question_downvote": -2,
+    "answer_upvote": 10,
+    "answer_downvote": -2,
+    "review_upvote": 3,
+    "review_downvote": -1,
+    "answer_accepted": 15,
+}
